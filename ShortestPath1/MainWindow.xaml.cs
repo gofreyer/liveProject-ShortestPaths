509f4eb63cce422f857853f5ffca2b2683451c9e
Geerd-Olaf Freyer <83983741+gofreyer@users.noreply.github.com>
2022-09-15 15:58:23 +0200

    Add files via upload

---
+++ ShortestPath1/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShortestPath1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FillNetwork(Network _network, List<int> x, List<int> y, List<string> label, List<int> f, List<int> t, List<int> c)
+        {
+            Node node;
+            for (int i = 0; i < x.Count; i++)
+            {
+                Point p = new Point(x[i], y[i]);
+                node = new Node(_network, p, label[i]);
+            }
+            Link link;
+            for (int j = 0; j < f.Count; j++)
+            {
+                Node from = _network.GetNode(f[j]);
+                Node to = _network.GetNode(t[j]);
+                if (from != null && to != null)
+                {
+                    link = new Link(_network, from, to, c[j]);
+                }
+            }
+        }
+        private void ValidateNetwork(Network _network, string _filename)
+        {
+            string networkTextOrg = _network.Serialization();
+            _network.SaveIntoFile(_filename);
+            _network.ReadFromFile(_filename);
+            string networkTextNew = _network.Serialization();
+            netTextBox.Text = networkTextNew;
+            if (networkTextOrg.Equals(networkTextNew))
+            {
+                statusLabel.Content = "OK";
+            }
+            else
+            {
+                statusLabel.Content = "Serializations do not match";
+            }
+        }
+
+        private void testButton1_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            2 # Num nodes.
+            1 # Num links.
+            # Nodes.
+            20,20,A
+            120,120,B
+            # Links.
+            0,1,10
+            */
+            List<int> nodes_x = new List<int> { 20,120};
+            List<int> nodes_y = new List<int> { 20,120 };
+            List<string> nodes_label = new List<string> { "A","B"};
+            List<int> links_from = new List<int> { 0 };
+            List<int> links_to = new List<int> { 1 };
+            List<int> links_cost = new List<int> {10 };
+            
+            Network net = new Network();
+            FillNetwork(net, nodes_x, nodes_y, nodes_label, links_from, links_to, links_cost);
+            ValidateNetwork(net, "c:\\Projekte\\LiveProject\\ShortestPath\\test1.net");
+
+        }
+
+        private void testButton2_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            4 # Num nodes.
+            4 # Num links.
+            # Nodes.
+            20,20,A
+            120,20,B
+            20,120,C
+            120,120,D
+            # Links.
+            0,1,10
+            1,3,15
+            0,2,20
+            2,3,25
+            */
+            List<int> nodes_x = new List<int> { 20, 120,20,120 };
+            List<int> nodes_y = new List<int> { 20, 20,120,120 };
+            List<string> nodes_label = new List<string> { "A", "B", "C", "D" };
+            List<int> links_from = new List<int> { 0,1,0,2 };
+            List<int> links_to = new List<int> { 1,3,2,3 };
+            List<int> links_cost = new List<int> { 10,15,20,25 };
+
+            Network net = new Network();
+            FillNetwork(net, nodes_x, nodes_y, nodes_label, links_from, links_to, links_cost);
+            ValidateNetwork(net, "c:\\Projekte\\LiveProject\\ShortestPath\\test2.net");
+        }
+
+        private void testButton3_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            4 # Num nodes.
+            8 # Num links.
+            # Nodes.
+            20,20,A
+            120,20,B
+            20,120,C
+            120,120,D
+            # Links.
+            0,1,10
+            1,3,15
+            0,2,20
+            2,3,25
+            1,0,11
+            3,1,16
+            2,0,21
+            3,2,26
+            */
+
+            List<int> nodes_x = new List<int> { 20, 120, 20, 120 };
+            List<int> nodes_y = new List<int> { 20, 20, 120, 120 };
+            List<string> nodes_label = new List<string> { "A", "B", "C", "D" };
+            List<int> links_from = new List<int> { 0, 1, 0, 2,1,3,2,3 };
+            List<int> links_to = new List<int> { 1, 3, 2, 3,0,1,0,2 };
+            List<int> links_cost = new List<int> { 10, 15, 20, 25,11,16,21,26 };
+
+            Network net = new Network();
+            FillNetwork(net, nodes_x, nodes_y, nodes_label, links_from, links_to, links_cost);
+            ValidateNetwork(net, "c:\\Projekte\\LiveProject\\ShortestPath\\test3.net");
+        }
+    }
+}
---
+++ ShortestPath1/Network.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace ShortestPath1
+{
+    class Node
+    {
+        public int Index { get; set; }
+        public Network MyNetwork { get; set; }
+        public Point Center { get; set; }
+        public string Text { get; set; }
+        public List<Link> MyLinks { get; set; }
+
+        public Node(Network _network, Point _center, string _text)
+        {
+            MyNetwork = _network;
+            Center = _center;
+            Text = _text;
+            Index = -1;
+            MyLinks = new List<Link>();
+            MyNetwork.AddNode(this);
+        }
+        public override string ToString()
+        {
+            return "[" + Text.ToString() + "]";
+        }
+        public void AddLink(Link _link)
+        {
+            MyLinks.Add(_link);
+        }
+    }
+    class Link
+    {
+        public int Index { get; set; }
+        public Network MyNetwork { get; set; }
+        public Node FromNode { get; set; }
+        public Node ToNode { get; set; }
+        public int Cost { get; set; }
+        public Link(Network _network, Node _from, Node _to, int _cost)
+        {
+            MyNetwork = _network;
+            FromNode = _from;
+            ToNode = _to;
+            Cost = _cost;
+            Index = -1;
+            FromNode.AddLink(this);
+            MyNetwork.AddLink(this);
+        }
+        public override string ToString()
+        {
+            return FromNode.ToString() + " --> " + ToNode.ToString() + " (" + Cost.ToString() + ")";
+        }
+    }
+    class Network
+    {
+        public List<Node> Nodes { get; set; }
+        public List<Link> Links { get; set; }
+        public Network()
+        {
+            Clear();
+        }
+        public void Clear()
+        {
+            Nodes = new List<Node>();
+            Links = new List<Link>();
+        }
+        public Node GetNode(int _index)
+        {
+            if (_index >= 0 && _index < Nodes.Count)
+            {
+                return Nodes[_index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int AddNode(Node _node)
+        {
+            int newIndex = Nodes.Count;
+            _node.Index = newIndex;
+            Nodes.Add(_node);
+            return newIndex;
+        }
+        public int AddLink(Link _link)
+        {
+            int newIndex = Links.Count;
+            _link.Index = newIndex;
+            Links.Add(_link);
+            return newIndex;
+        }
+        public string Serialization()
+        {
+            string result = "";
+            result += $"{Nodes.Count} # Num nodes.\n";
+            result += $"{Links.Count} # Num links.\n";
+            result += "# Nodes.\n";
+            foreach (Node node in Nodes)
+            {
+                result += $"{node.Center.X},{node.Center.Y},{node.Text}\n";
+            }
+
+            result += "# Links.\n";
+            foreach (Link link in Links)
+            {
+                result += $"{link.FromNode.Index},{link.ToNode.Index},{link.Cost}\n";
+            }
+            
+            return result;
+        }
+        public void SaveIntoFile(string _filename)
+        {
+            string networkText = Serialization();
+            try
+            {
+                File.WriteAllText(_filename, networkText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public string ReadNextLine(StringReader _stringreader)
+        {
+            while(true)
+            {
+                string line = _stringreader.ReadLine();
+                if (line == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    string[] tokens = line.Split('#');
+                    foreach (string token in tokens)
+                    {
+                        line = token.Trim();
+                        if (line.Length > 0)
+                        {
+                            return line;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        public void Deserialize(string _serialization)
+        {
+            Clear();
+            using (StringReader reader = new StringReader(_serialization))
+            {
+                int nodeCount = 0;
+                int linkCount = 0;
+                string line = ReadNextLine(reader);
+                if (!int.TryParse(line, out nodeCount) || (nodeCount <= 0))
+                {
+                    throw new InvalidOperationException("NodesCount invalid");
+                }
+                line = ReadNextLine(reader);
+                if (!int.TryParse(line, out linkCount) || (linkCount < 0))
+                {
+                    throw new InvalidOperationException("LinksCount invalid");
+                }
+                for (int n = 0; n < nodeCount; n++)
+                {
+                    line = ReadNextLine(reader);
+                    if (line != null)
+                    {
+                        string[] tokens = line.Split(',');
+                        if (tokens.Length != 3)
+                        {
+                            throw new InvalidOperationException("Node invalid");
+                        }
+                        Point p = new Point(Int32.Parse(tokens[0]), Int32.Parse(tokens[1]));
+                        Node node = new Node(this, p, tokens[2]);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Node invalid");
+                    }
+                }
+                for (int l = 0; l < linkCount; l++)
+                {
+                    line = ReadNextLine(reader);
+                    if (line != null)
+                    {
+                        string[] tokens = line.Split(',');
+                        if (tokens.Length != 3)
+                        {
+                            throw new InvalidOperationException("Link invalid");
+                        }
+                        Node from = GetNode(Int32.Parse(tokens[0]));
+                        Node to = GetNode(Int32.Parse(tokens[1]));
+                        int cost = Int32.Parse(tokens[2]);
+                        if (from != null && to != null && cost >= 0)
+                        {
+                            Link link = new Link(this, from, to, cost);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Link invalid");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Link invalid");
+                    }
+                }
+            }
+        }
+        public void ReadFromFile(string _filename)
+        {
+            string networkText = "";
+            try
+            {
+                networkText = File.ReadAllText(_filename);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                Deserialize(networkText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
